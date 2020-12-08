d5d9852e538973d0268c9a5418f3bf82cd43a053
Matthew Gaddes <matthew.gaddes@gmail.com>
2020-12-08 14:36:48 -0800

    Add error handling to 'create' form

--- components/Create/Create.js
+++ components/Create/Create.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 import { useForm } from 'react-hook-form';
+import isNil from 'lodash/isNil';
+import isEmpty from 'lodash/isEmpty';
 
 import { addTask } from '../../data/reducers/taskSlice';
 
@@ -17,10 +19,24 @@ export default function Create() {
     register('description');
     register('day');
     register('priority');
-  }, [register])
+  }, [register]);
+
+  // Helper to determine if newly-created task has all required fields.
+  // Returns true if any of the "invalid" conditions are met.
+  const taskIsInvalid = data =>
+    isNil(data.title) ||
+    isEmpty(data.title) ||
+    isNil(data.day) ||
+    isNil(data.priority);
 
   const onSubmit = data => {
-    console.log({ data });
+    if (taskIsInvalid(data)) {
+      Alert.alert(
+        'Error',
+        'Please enter all required fields',
+      );
+      return;
+    }
     dispatch(addTask(data));
   };
 
