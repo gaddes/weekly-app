ed0e3e3527fe5965b5e2a394dfe70023566112cc
Matthew Gaddes <matthew.gaddes@gmail.com>
2021-01-03 17:19:28 -0800

    Add swipeable row

--- components/Current/Item.js
+++ components/Current/Item.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import { SwipeableRow } from '../common';
 import Toggle from './Toggle';
 import Title from './Title';
 import Description from './Description';
@@ -10,33 +11,35 @@ export default function ListItem(props) {
   const [editing, setEditing] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.item}>
-        <Toggle
-          id={props.id}
-          priority={props.priority}
-          completed={props.completed}
-        />
+    <SwipeableRow>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <Toggle
+            id={props.id}
+            priority={props.priority}
+            completed={props.completed}
+          />
 
-        <Title
-          title={props.title}
+          <Title
+            title={props.title}
+            description={props.description}
+            completed={props.completed}
+            editing={editing}
+            setEditing={setEditing}
+          />
+
+          <Delete
+            id={props.id}
+            editing={editing}
+          />
+        </View>
+
+        <Description
           description={props.description}
-          completed={props.completed}
-          editing={editing}
-          setEditing={setEditing}
-        />
-
-        <Delete
-          id={props.id}
           editing={editing}
         />
       </View>
-
-      <Description
-        description={props.description}
-        editing={editing}
-      />
-    </View>
+    </SwipeableRow>
   );
 }
 
@@ -47,6 +50,12 @@ const styles = StyleSheet.create({
     flexWrap: 'nowrap',
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
+    // TODO: update this!
+    // Hacky way to match the current (and very boring) background color
+    backgroundColor: 'rgb(242, 242, 242)',
+    padding: 16,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 
   item: {
