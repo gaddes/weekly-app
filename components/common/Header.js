d5dc7d7229ab3147510cb3840319517de1b757d8
Matthew Gaddes <matthew.gaddes@gmail.com>
2021-01-14 17:23:51 -0800

    Integrate data on purchase screen with RevenueCat

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import Purchases from 'react-native-purchases';
 import Feather from '@expo/vector-icons/Feather';
+import isNil from 'lodash/isNil';
+import isEmpty from 'lodash/isEmpty';
 
 import { Upgrade } from '../Upgrade';
 
@@ -12,10 +15,31 @@ const handlePress = () => {
 
 export default function Header(props) {
   const [upgradeVisible, setUpgradeVisible] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const handlePressUpgrade = async () => {
+    try {
+      const offerings = await Purchases.getOfferings();
+
+      if (isNil(offerings.current) || isEmpty(offerings.current.availablePackages)) {
+        // Show error message and return early if offerings can't be retrieved
+        Alert.alert('Upgrade options could not be retrieved');
+        return;
+      }
+
+      // Set products in state so modal has access to them
+      setProducts(offerings.current.availablePackages);
+
+      // Show modal
+      setUpgradeVisible(true);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <View style={styles.header}>
-      <TouchableOpacity onPress={() => setUpgradeVisible(true)}>
+      <TouchableOpacity onPress={handlePressUpgrade}>
         <Feather
           style={{ marginLeft: 12 }}
           name="arrow-up-circle"
@@ -38,6 +62,7 @@ export default function Header(props) {
       <Upgrade
         visible={upgradeVisible}
         setVisible={setUpgradeVisible}
+        products={products}
       />
     </View>
   );
